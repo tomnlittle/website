9593177d8c873b15d0db00d1e6d9e9ea87b7e1e7
Thomas Northall-Little <tomnlittle@outlook.com>
2017-11-15 09:49:29 +1100

    Added some basic styling

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// Semantic UI styles
+require('semantic-ui-css/semantic.min.css');
+
 //Import the scss file
 require('./stylesheets/main.scss');
 
