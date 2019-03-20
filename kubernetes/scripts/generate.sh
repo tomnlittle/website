c40316126925c7bbff66b095e25f0b0e22483129
Thomas Northall-Little <tomnlittle@outlook.com>
2019-03-20 19:53:13 +1100

    kubernetes configurations for dropbox api

--- kubernetes/scripts/generate.sh
+++ kubernetes/scripts/generate.sh
@@ -1,7 +1,7 @@
 mkdir $PWD/templates
 
 helm template \
-  --set tag=$TAG \
+  --set tag=$TAG,backend.dropboxAccessToken=$DROPBOX_ACCESS_TOKEN \
   --name website \
   --namespace default \
   --values $PWD/config.yaml \
