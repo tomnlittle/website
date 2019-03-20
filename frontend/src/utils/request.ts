aa16229791a44f2a01f7b50242e9ddfed2467f22
Thomas Northall-Little <tomnlittle@outlook.com>
2019-03-20 19:50:44 +1100

    query string requests on frontend

--- frontend/src/utils/request.ts
+++ frontend/src/utils/request.ts
@@ -5,13 +5,27 @@ interface IMethodArguments {
   body?: {};
 }
 
+function queryObjectToString(data: any) {
+  // http://stackoverflow.com/a/34209399/707580
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&');
+}
+
 export async function request({
   url,
   method = 'GET',
   qs,
   body
 }: IMethodArguments): Promise<Response> {
-  return fetch(url, {
+
+  let uri = url;
+
+  if (qs) {
+    uri += '?' + queryObjectToString(qs);
+  }
+
+  return fetch(uri, {
     method,
     headers: {
       Accept: 'application/json'
