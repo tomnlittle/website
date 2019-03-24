4588645970a8a28f33d1fda66e4e57d4d69e4319
Thomas Northall-Little <tomnlittle@outlook.com>
2019-03-24 21:05:01 +1100

    linkedIn and GitHub links

--- frontend/src/Landing/Landing.tsx
+++ frontend/src/Landing/Landing.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import Button from "./Button/Button";
 import "./Landing.css";
-import ProfilePic from "./profile.jpg";
+import ProfilePic from "./Profile.jpg";
 import SideImage from "./SideImage.jpg";
 
 export default class Landing extends React.Component {
@@ -16,9 +16,15 @@ export default class Landing extends React.Component {
         <div className={"Left"}>
           <img src={ProfilePic} className="Profile-Pic" alt={"Profile Picture"}/>
           <h1>Thomas Northall-Little</h1>
-          <p className="Contact"> <a href="mailto:contact@tomnlittle.com"> contact@tomnlittle.com </a> </p>
+
+          <p> <a href="mailto:contact@tomnlittle.com"> contact@tomnlittle.com </a> </p>
+          <p className="Links">
+            <a href="https://www.linkedin.com/in/thomas-northall-little-073604111">LinkedIn</a>
+            <a href="https://github.com/tomnlittle">GitHub</a>
+          </p>
 
           <Button> Project Timeline </Button>
+          <Button> Experience </Button>
         </div>
 
         {/* Right Side */}
